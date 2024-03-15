3ffb847318e9f9f52b004a9dc6ed08ef55ededbc
Zhoholiev <pzhogolev@perfsys.com>
2024-03-15 13:00:08 +0200

    remove weren't use imports 15.03.24 12:59 Zhoholiev P

--- src/app/utils/SearchUser.java
+++ src/app/utils/SearchUser.java
@@ -2,10 +2,8 @@ package app.utils;
 
 import app.entitys.User;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
